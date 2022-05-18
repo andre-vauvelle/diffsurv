abef701d2972cd64108d999bca963182900dbf3f
rmhivau <rmhivau@node-j00a-001.myriad.ucl.ac.uk>
2022-05-18 09:50:38 +0100

    add myriad support

---
+++ src/jobs/myriad_imports.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh -l
+hostname
+date
+# SOURCE project
+SOURCE_DIR='/home/rmhivau/ehrgnn/'
+export PYTHONPATH=$PYTHONPATH:$SOURCE_DIR
+cd $SOURCE_DIR/src/ || exit
+
+# load cuda
+bash
+module -f unload compilers mpi gcc-libs
+module load beta-modules
+module load gcc-libs/10.2.0
+module load compilers/gnu/10.2.0
+module load cuda/11.3.1/gnu-10.2.0
+conda activate ehrgnn
+zsh
+
