211d3ab502ab149514f9e0d0d15540dcf6f12a31
rmhivau <rmhivau@node-h00a-007.myriad.ucl.ac.uk>
2022-06-09 00:29:45 +0100

    update callback

--- src/callbacks.py
+++ src/callbacks.py
@@ -12,35 +12,36 @@ class LogPredictionsCallback(Callback):
     def on_validation_batch_end(
             self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
         """Called when the validation batch ends."""
+        if not batch_idx % 20:
 
-        wandb_logger = trainer.logger
+            wandb_logger = trainer.logger
 
-        # `outputs` comes from `LightningModule.validation_step`
-        loss, predictions, perm_prediction, perm_ground_truth = outputs
+            # `outputs` comes from `LightningModule.validation_step`
+            loss, predictions, perm_prediction, perm_ground_truth = outputs
 
-        # Let's log 20 sample image predictions from first batch
+            # Let's log 20 sample image predictions from first batch
 
-        label_times = batch['label_times']
+            label_times = batch['label_times']
 
-        idx = torch.argsort(label_times.squeeze(), descending=False)
-        perm_ascending = torch.nn.functional.one_hot(idx).transpose(-2, -1).float()
-        perm_prediction_asc = perm_ascending.T @ perm_prediction
-        perm_ground_truth_asc = perm_ascending.T @ perm_ground_truth
+            idx = torch.argsort(label_times.squeeze(), descending=False)
+            perm_ascending = torch.nn.functional.one_hot(idx).transpose(-2, -1).float()
+            perm_prediction_asc = perm_ascending.T @ perm_prediction
+            perm_ground_truth_asc = perm_ascending.T @ perm_ground_truth
 
-        captions = ["Soft Permutation", "Predicted Permutation"]
+            captions = ["Soft Permutation", "Predicted Permutation"]
 
-        wandb_logger.log_image(key=f'batch:{batch_idx}', images=[perm_ground_truth_asc, perm_prediction_asc],
-                               caption=captions)
+            wandb_logger.log_image(key=f'batch:{batch_idx}', images=[perm_ground_truth_asc, perm_prediction_asc],
+                                   caption=captions)
 
-        # lt_dist = torch.nn.functional.normalize(label_times.squeeze()[idx], dim=0)
-        # pred_dist = torch.nn.functional.normalize(predictions[idx], dim=0)
-        #
-        # data = list(zip(lt_dist, pred_dist))
-        # table = wandb.Table(data=data, columns=["label", "value"])
-        # wandb_logger.({"my_bar_chart_id": wandb.plot.bar(table, "label", "value", title="Custom Bar Chart")})
-        #
-        # torch_show([perm_ground_truth_asc, perm_prediction_asc])
-        # plt.show()
+            # lt_dist = torch.nn.functional.normalize(label_times.squeeze()[idx], dim=0)
+            # pred_dist = torch.nn.functional.normalize(predictions[idx], dim=0)
+            #
+            # data = list(zip(lt_dist, pred_dist))
+            # table = wandb.Table(data=data, columns=["label", "value"])
+            # wandb_logger.({"my_bar_chart_id": wandb.plot.bar(table, "label", "value", title="Custom Bar Chart")})
+            #
+            # torch_show([perm_ground_truth_asc, perm_prediction_asc])
+            # plt.show()
 
 
 log_predictions_callback = LogPredictionsCallback()
