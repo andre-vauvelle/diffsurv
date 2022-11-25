f4536516c86aab4d0a2844ba310603e196262663
Andre Vauvelle <vauvelleandre@gmail.com>
2022-11-25 17:29:21 +0000

    udpate imports and add comment for log eveery n

--- src/jobs/experiments/sweep_agent.sh
+++ src/jobs/experiments/sweep_agent.sh
@@ -10,12 +10,17 @@
 
 hostname
 date
-SOURCE_DIR='/home/vauvelle/diffsurv/'
+SOURCE_DIR='/home/vauvelle/diffsurv/src'
 export PYTHONPATH=$PYTHONPATH:$SOURCE_DIR
-cd $SOURCE_DIR/src/ || exit
-source /share/apps/source_files/cuda/cuda-10.1.source
-#source ~/.bashrc
-conda activate
+cd $SOURCE_DIR || exit
+source ~/.bashrc
+
+# These are may need to be reset for local installs...
+export PATH=/share/apps/python-3.9.5-shared/bin:${PATH}
+export LD_LIBRARY_PATH=/share/apps/python-3.9.5-shared/lib:${LD_LIBRARY_PATH}
+export PATH=/usr/local/cuda/bin:${PATH} # CUDA 11.5
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:${LD_LIBRARY_PATH}
+export PATH=/home/vauvelle/.local/bin:${PATH}
 
 #python scripts/bert_mlm.py fit --config="${CONFIG_FILE:=jobs/configs/mlm/bert_phecode.yaml}"
 echo Using command:
