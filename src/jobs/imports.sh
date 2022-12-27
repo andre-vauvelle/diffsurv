5c5c7e2ed964cde592362e58c05e8dcf98f0c5b2
HPC Andre <vauvelleandre@gmail.com>
2022-12-27 20:31:26 +0000

    add env var for wanb dir

--- src/jobs/imports.sh
+++ src/jobs/imports.sh
@@ -11,3 +11,5 @@ export LD_LIBRARY_PATH=/share/apps/python-3.9.5-shared/lib:${LD_LIBRARY_PATH}
 export PATH=/usr/local/cuda/bin:${PATH} # CUDA 11.5
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:${LD_LIBRARY_PATH}
 export PATH=/home/vauvelle/.local/bin:${PATH}
+export WANDB_DIR=/SAN/ihibiobank/denaxaslab/andre/diffsurv/results/wandb/
+
