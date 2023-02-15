2372f26e6667c53607054113bdf93c92db1b1446
Andre Vauvelle <vauvelleandre@gmail.com>
2023-02-15 18:10:01 +0000

    add checkpointing

--- src/models/imaging.py
+++ src/models/imaging.py
@@ -8,10 +8,23 @@ class SVHNConvNet(nn.Module):
         scale = img_size / 48
         fc1_size = int(3 * 3 * 256 * scale**2)
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, 5, 1, 2)
-        self.conv2 = nn.Conv2d(32, 64, 5, 1, 2)
-        self.conv3 = nn.Conv2d(64, 128, 5, 1, 2)
-        self.conv4 = nn.Conv2d(128, 256, 5, 1, 2)
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(3, 32, 5, 1, 2),
+            nn.ReLU(),
+        )
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(32, 64, 5, 1, 2),
+            nn.ReLU(),
+        )
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(64, 128, 5, 1, 2),
+            nn.ReLU(),
+        )
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(128, 256, 5, 1, 2),
+            nn.ReLU(),
+        )
+
         self.fc1 = nn.Linear(fc1_size, 64)
         self.fc2 = nn.Linear(64, 1)
 
@@ -19,13 +32,10 @@ class SVHNConvNet(nn.Module):
         x_shape = x.shape
         if len(x_shape) == 5:
             x = x.view(-1, *x_shape[-3:])
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv3(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv4(x))
+        x = torch.utils.checkpoint.checkpoint(self.convblock1, x)
+        x = torch.utils.checkpoint.checkpoint(self.convblock2, x)
+        x = torch.utils.checkpoint.checkpoint(self.convblock3, x)
+        x = torch.utils.checkpoint.checkpoint(self.convblock4, x)
         x = F.max_pool2d(x, 2, 2)
         x = x.view(*x_shape[:-3], -1)
         x = F.relu(self.fc1(x))
