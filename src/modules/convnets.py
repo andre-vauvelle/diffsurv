78df533203351c4c2640cb029b342c06147745cb
Andre Vauvelle <vauvelleandre@gmail.com>
2023-02-16 15:31:15 +0000

    add convnext

--- src/modules/convnets.py
+++ src/modules/convnets.py
@@ -23,12 +23,12 @@ class ConvModule(BaseModel):
         elif model == "convnext_small":
             self.conv_net = convnext_small(pretrained=True)
             self.conv_net.classifier = nn.Sequential(
-                norm_layer(768), nn.Flatten(1), nn.Linear(768, 1, bias=False)
+                nn.Flatten(1), norm_layer(768), nn.Linear(768, 1, bias=False)
             )
         elif model == "convnext_tiny":
             self.conv_net = convnext_tiny(pretrained=True)
             self.conv_net.classifier = nn.Sequential(
-                norm_layer(768), nn.Flatten(1), nn.Linear(768, 1, bias=False)
+                nn.Flatten(1), norm_layer(768), nn.Linear(768, 1, bias=False)
             )
         else:
             raise ValueError(
