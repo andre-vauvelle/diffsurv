a73c551f11bf733039a846d8ed85fe2ed61da6b2
Andre Vauvelle <vauvelleandre@gmail.com>
2023-04-19 21:57:44 +0100

    update k folds with two configs and new seed

--- src/scripts/k_folds.py
+++ src/scripts/k_folds.py
@@ -13,11 +13,23 @@ from scripts.mlpdiffsort import diffsort_cli_main
 
 parser = argparse.ArgumentParser(description="Run K Folds of training and validation for a model")
 parser.add_argument("-k", "--kfolds", type=int, default=5)
-parser.add_argument("-c", "--config", type=str)
+parser.add_argument("-c", "--config", action="append", type=str, required=True)
 parser.add_argument("-m", "--model", type=str, default="diffsort")
 parser.add_argument("-d", "--results_dir", type=str)
 
-seed_everything(seed=42)
+seed_everything(seed=43)
+
+
+def load_yaml_file(file_path):
+    with open(file_path) as file:
+        return yaml.safe_load(file)
+
+
+def combine_dicts(dict1, dict2):
+    combined_dict = dict1.copy()
+    combined_dict.update(dict2)
+    return combined_dict
+
 
 args = parser.parse_args()
 
@@ -40,11 +52,16 @@ elif args.model == "mlp":
 else:
     raise NotImplementedError("model must be one of the implmented {'mlp', 'diffsort'}")
 
+# Load and combine all configuration files
+combined_configuration = {}
+for config_file in args.config:
+    config_data = load_yaml_file(config_file)
+    combined_configuration = combine_dicts(combined_configuration, config_data)
+
+configuration = combined_configuration
+
 metrics_store = []
 for k in range(args.kfolds):
-    with open(args.config) as file:
-        configuration = yaml.safe_load(file)
-
     configuration["data"]["k_fold"] = (k, args.kfolds)
 
     cli = model_cli(args=configuration, run=False)
